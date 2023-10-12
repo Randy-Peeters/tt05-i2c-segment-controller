8318a28f117661409db57700204573428b8223dd
Randy Peeters <77305431+Randy-Peeters@users.noreply.github.com>
2023-10-12 21:13:38 +0200

    Update tt_um_randypeeters-i2c-segment-controller-top.v

--- src/tt_um_randypeeters-i2c-segment-controller-top.v
+++ src/tt_um_randypeeters-i2c-segment-controller-top.v
@@ -1,4 +1,4 @@
-module tt_um_randypeeters-i2c-segment-controller-top.v (
+module tt_um_randypeeters-i2c-segment-controller-top (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
