c3070483ba966ac25210f29f476fb6a240a79d57
Randy <randypeeters74@gmail.com>
2023-10-19 19:37:46 +0200

    LOL

---
+++ src/segment_logic.v
@@ -0,0 +1,9 @@
+`default_nettype 
+
+module segment_logic (
+    input wire clk,
+    input wire rst_n,
+    input wire 
+);
+    
+endmodule
--- src/spi.v
+++ src/spi.v
@@ -1,13 +1,15 @@
 `default_nettype none
 
 module spi(
-    input wire rst_n,
-    input wire cs_n,
-    input wire sck,
-    input wire mosi,
-    output reg miso
+    input wire  rst_n,
+    input wire  cs_n,
+    input wire  sck,
+    input wire  mosi,
+    output reg  miso,
+    output wire data_reg_0x69
 );
 
+reg [7:0] data_buff;
 reg [8:0] shift_reg_in;
 reg [3:0] bit_count;
 
@@ -22,8 +24,16 @@ always @(posedge(sck) or negedge(rst_n)) begin
 
     //If the chip is selected.
     else if(cs_n == 1'b0) begin
-        shift_reg_in[8] <= mosi;
-        shift_reg_in = shift_reg_in >> 1;
+        if (bit_count == 8) begin
+            bit_count = 0;
+            data_buff = shift_reg_in[7:0];
+        end
+        else begin
+            bit_count = bit_count + 1;
+            shift_reg_in[8] <= mosi;
+            shift_reg_in = shift_reg_in >> 1;
+        end
+
     end
 end
 
